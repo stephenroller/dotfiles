e0a37d26d880c06cc68245661312657d1209d488
Stephen Roller <scroller@ncsu.edu>
2008-05-19 19:56:40 -0400

    Updating deploy file.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-for dotfile in .bashrc .hgrc .screenrc .vimrc .hgext
+for dotfile in .bashrc .hgrc .screenrc .vimrc
 do
 	ln -fs "`pwd`/$dotfile" ~/$dotfile
 done
+
+rmdir ~/.hgext 2>/dev/null
+rm ~/.hgext 2>/dev/null
+ln -s `pwd`/.hgext ~/
 
