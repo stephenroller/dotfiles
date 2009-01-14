b2bfe33fb3d52a3fbbd76e7b66a4b0985953def1
Stephen Roller <scroller@ncsu.edu>
2009-01-13 22:52:43 -0500

    Adding gitconfig.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for dotfile in .bashrc .hgrc .screenrc .vimrc .inputrc .shellprompt.py .pythonrc.py
+for dotfile in .bashrc .hgrc .screenrc .vimrc .inputrc .shellprompt.py .pythonrc.py .gitconfig
 do
 	ln -fs "`pwd`/$dotfile" ~/$dotfile
 done
