042f821aa6bcebb91fbebc0d82d3b5191178370a
Stephen Roller <scroller@ncsu.edu>
2009-01-13 23:14:01 -0500

    Adding ssh_config

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,15 @@ do
 	ln -fs "`pwd`/$dotfile" ~/$dotfile
 done
 
+# handle ssh config
+if [ ! -d ~/.ssh ]; then
+
+	mkdir -p ~/.ssh
+	chmod 0700 ~/.ssh
+fi
+ln -fs "`pwd`/.sshconfig" ~/.ssh/config
+
+# and handle special mercurial extensions
 rmdir ~/.hgext 2>/dev/null
 rm ~/.hgext 2>/dev/null
 ln -s `pwd`/.hgext ~/
