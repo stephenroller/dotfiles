7c7d05659a6b63cd782a252896df0315159ba169
Stephen Roller <roller@character.ai>
2023-05-01 20:07:50 +0000

    Don't deploy ssh config, always messes me up.

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,3 @@ done
 if [ ! -s ~/.vim ]; then
     ln -s "`pwd`/.vim" ~/.vim
 fi
-
-# handle ssh config
-if [ ! -d ~/.ssh ]; then
-
-	mkdir -p ~/.ssh
-	chmod 0700 ~/.ssh
-fi
-ln -fs "`pwd`/.sshconfig" ~/.ssh/config
-
